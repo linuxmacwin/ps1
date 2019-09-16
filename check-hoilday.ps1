c5352a53e681577a2023ac35720ebce68cdbf236
linuxmacwin <52692365+linuxmacwin@users.noreply.github.com>
2019-09-16 13:49:33 +0800

    Add files via upload

---
+++ check-hoilday.ps1
@@ -0,0 +1,7 @@
+Get-FileHash -Algorithm MD5 D:\hx\shex-ldds\conf\holiday.ini|Out-File D:\holiday.txt
+Get-FileHash -Algorithm MD5 D:\hx\shhk\conf\holiday.ini|Out-File -Append D:\holiday.txt
+Get-FileHash -Algorithm MD5 D:\hx\szsel\conf\holiday.ini|Out-File -Append D:\holiday.txt
+Get-FileHash -Algorithm MD5 D:\hx\hexinvss\conf\holiday.ini|Out-File -Append D:\holiday.txt
+Get-FileHash -Algorithm MD5 D:\hx\zzindex\conf\holiday.ini|Out-File -Append D:\holiday.txt
+Get-FileHash -Algorithm MD5 D:\hx\shsz-console\conf\holiday.ini|Out-File -Append D:\holiday.txt
+Get-FileHash -Algorithm MD5 D:\hx\shszcalc\conf\holiday.ini|Out-File -Append D:\holiday.txt
